61b2be7fc3781a8404b55240030540719efbd984
ScrollengineOne <solentha570@Gmail.com>
2025-07-31 11:54:47 -0400

    Update plot.py

--- utils/plot.py
+++ utils/plot.py
@@ -1,1 +1,14 @@
+# utils/plot.py
+
+import matplotlib.pyplot as plt
+
+def plot_waveform(wave, title="Scrolltone Waveform"):
+    plt.figure(figsize=(10, 4))
+    plt.plot(wave, color='orange', linewidth=1.2)
+    plt.title(title)
+    plt.xlabel("Time Step")
+    plt.ylabel("Amplitude")
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
 

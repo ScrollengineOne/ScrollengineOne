d4e14b8a32b7ff6ee37b8fea077527abf3c6c791
ScrollengineOne <solentha570@Gmail.com>
2025-07-31 11:49:27 -0400

    Update oscillator.py

--- core/oscillator.py
+++ core/oscillator.py
@@ -1,1 +1,21 @@
+# core/oscillator.py
+
+import numpy as np
+from scipy.integrate import odeint
+
+def harmonic_oscillator(state, t, params):
+    x, v = state
+    m = params['mass']
+    k = params['spring_const']
+    c = params['damping']
+    F = params['drive_amp'] * np.cos(params['drive_freq'] * t + params['drive_phase'])
+
+    dxdt = v
+    dvdt = (F - c * v - k * x) / m
+    return [dxdt, dvdt]
+
+def run_simulation(params, t):
+    init_state = [0.0, 0.0]
+    sol = odeint(harmonic_oscillator, init_state, t, args=(params,))
+    return sol[:, 0]  # return displacement only
 
